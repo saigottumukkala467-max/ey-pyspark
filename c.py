f1cd2028802632e678bd107e6734b3e9d76b10fc
saigottumukkala467-max <saigottumukkala467@gmail.com>
2026-03-02 14:25:59 +0530

    Add comment to c.py

--- c.py
+++ c.py
@@ -1,4 +1,4 @@
-
+#hiii
 from pyspark.sql import SparkSession, functions as F
 spark = SparkSession.builder.getOrCreate()
 spark.read.csv("data.csv", header=True, inferSchema=True).withColumn("total", F.col("a")+F.col("b")).filter(F.col("total")>0).write.csv("out.csv", header=True, mode="overwrite")
