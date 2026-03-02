01c4b48d470266ecc966916082f2aced33388191
Sai Naga Sruthi Gottumukkala R D <sai.naga.sruthi.gottumukkala.r.d@gds.ey.com>
2026-03-02 14:24:09 +0530

    first commit

--- c.py
+++ c.py
@@ -1,1 +1,4 @@
-print("hi")
+
+from pyspark.sql import SparkSession, functions as F
+spark = SparkSession.builder.getOrCreate()
+spark.read.csv("data.csv", header=True, inferSchema=True).withColumn("total", F.col("a")+F.col("b")).filter(F.col("total")>0).write.csv("out.csv", header=True, mode="overwrite")
